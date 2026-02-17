d7b9a78ac49cf68add2a1089742882f1d2cd29a7
ArnauFabregat <arnaufara89@gmail.com>
2026-02-17 16:52:01 +0100

    small msg fix

--- src/talent_selection_flow/flow.py
+++ src/talent_selection_flow/flow.py
@@ -147,8 +147,8 @@ class TalentSelectionFlow(Flow[TalentState]):
 
     @listen("route_other")
     def handle_other(self) -> None:
-     msg = f"Invalid document type: '{self.state.input_type}'. Expected '{DocumentType.CV}' or '{DocumentType.JOB}'. " \
-            "Please, start new evaluation."
+     msg = f"Invalid document type. Expected '{DocumentType.CV}' or '{DocumentType.JOB}'. " \
+            "Please, start a new evaluation."
      logger.warning(msg)
      return msg
 
